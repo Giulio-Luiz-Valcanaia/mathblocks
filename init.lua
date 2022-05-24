e7805a3fe12eb9066f5a46a7a456264d4be1a454
Giulio-Luiz-Valcanaia <67170840+Giulio-Luiz-Valcanaia@users.noreply.github.com>
2022-05-24 06:43:40 -0300

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,71 @@
+minetest.register_node("mathblocks:one_to_the_power_of_two", {
+    description = "1^2",
+    tiles = {"one_to_the_power_of_two.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("mathblocks:one_to_the_power_of_three", {
+    description = "1^3",
+    tiles = {"one_to_the_power_of_three.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("mathblocks:two_to_the_power_of_two", {
+    description = "2^2",
+    tiles = {"two_to_the_power_of_two.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("mathblocks:two_to_the_power_of_three", {
+    description = "2^3",
+    tiles = {"two_to_the_power_of_three.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("mathblocks:three_to_the_power_of_two", {
+    description = "3^2",
+    tiles = {"three_to_the_power_of_two.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("mathblocks:three_to_the_power_of_three", {
+    description = "3^3",
+    tiles = {"three_to_the_power_of_three.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("mathblocks:four_to_the_power_of_two", {
+    description = "4^2",
+    tiles = {"four_to_the_power_of_two.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("mathblocks:four_to_the_power_of_three", {
+    description = "4^3",
+    tiles = {"four_to_the_power_of_three.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("mathblocks:five_to_the_power_of_two", {
+    description = "5^2",
+    tiles = {"five_to_the_power_of_two.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("mathblocks:five_to_the_power_of_three", {
+    description = "5^3",
+    tiles = {"five_to_the_power_of_three.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("mathblocks:six_to_the_power_of_two", {
+    description = "6^2",
+    tiles = {"six_to_the_power_of_two.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("mathblocks:six_to_the_power_of_three", {
+    description = "6^3",
+    tiles = {"six_to_the_power_of_three.png"},
+    groups = {cracky = 1}
+})
